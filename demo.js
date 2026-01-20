922c27a5a1e6310ec358dbf675b00999f91bd930
Nupur <nupurkantiya@gmail.com>
2026-01-20 11:24:00 +0530

    add initial demo script with console logs

---
+++ demo.js
@@ -0,0 +1,10 @@
+console.log("Hello Coders!");
+console.log("Welcome to the coding demo.");
+var a = 78;
+console.log("Print: " + a);
+var b = 45;
+console.log("Both numbers are: " + a + " and " +b);
+
+
+
+
